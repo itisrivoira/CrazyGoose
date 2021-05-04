7f2c53601c38fcf864df8640a66abbf48b686362
Alessandro Dastru <58624857+Adidastru@users.noreply.github.com>
2021-05-04 11:40:13 +0200

    email funzionante...

--- server_nodejs/sitoWeb/phpFiles/send.php
+++ server_nodejs/sitoWeb/phpFiles/send.php
@@ -1,8 +1,9 @@
 <?php
-   $to = "crazygoose.help@gmail.com";
+   $to = 'crazygoose.help@gmail.com';
 
    // Oggetto della mail
    $subject = 'INFO';
+   
    $nome = $_POST['nome']; 
    $cognome = $_POST['cognome'];
    $domanda = $_POST['testo'];
@@ -10,23 +11,26 @@
    $message = '
     <html>
     <head>
-        <title>Domanda da: <?php $nome , $cognome?></title>
+        <title>Domanda da: '.$nome.' '.$cognome.'</title>
     </head>
     <body>
-        <p><?php $domanda?></p>
+        <p>'.$domanda.'</p>
     </body>
     </html>
-';;
+';
    
    // Per inviare una mail HTML, deve essere opportunamente settato il Content-type dell'header
    $headers[] = 'MIME-Version: 1.0';
    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
    $emailUser = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-   $headers[] = 'From: '.$emailUser;
-   $headers[] = $emailUser;
+   //echo "BOH: ".$emailUser." Nome: ".$nome." Cognome: ".$cognome." Domanda: $domanda";
+   //$headers[] = 'From: '.$emailUser;
+   $headers[] = 'To: Crazy Goose team <'.$to.'>';
+   $headers[] = 'From: '.$cognome.' <'.$emailUser.'>';
+   //$headers[] = $emailUser;
     
    // Inviamo la mail!
    mail($to, $subject, $message, implode("\r\n", $headers));
-   echo "<br>Ok fatto!"; 
+   echo "<script type='text/javascript'>alert('Email inviata!');</script>"; 
 ?>
